4cc8d51237948c02de43314f36b2183bdfe10458
Jean Robertou <j-rob@live.fr>
2024-12-25 11:03:43 +0100

    feat: display resources in columns

--- src/app/(public)/(landing)/components/Stats.tsx
+++ src/app/(public)/(landing)/components/Stats.tsx
@@ -4,11 +4,18 @@ import { RootState } from '@/store/store'
 import { useSelector } from 'react-redux'
 
 export const Stats = () => {
-  const { watt } = useSelector((state: RootState) => state.dashboard)
+  const { watt, woodChoppers } = useSelector(
+    (state: RootState) => state.dashboard
+  )
 
   return (
     <div className="rounded-sm border border-white px-4 py-2">
-      {watt.toFixed(0)} watts
+      <div className="flex items-center justify-between">
+        <p>Watts {watt.toFixed(0)}</p>
+        {woodChoppers ? (
+          <p className="text-right text-zinc-500">+ {woodChoppers}/s</p>
+        ) : null}
+      </div>
     </div>
   )
 }
--- src/app/(public)/(landing)/page.tsx
+++ src/app/(public)/(landing)/page.tsx
@@ -7,17 +7,19 @@ import { Stats } from './components/Stats'
 export default function Home() {
   return (
     <div className="min-h-screen space-y-12 py-12">
-      <Container size="xsmall">
-        <div className="flex items-start justify-between">
+      <Container size="small">
+        <div className="grid h-screen grid-cols-3 gap-4">
           <div>
+            <Stats />
+          </div>
+
+          <div className="col-span-2">
             <Generators />
             <ChopWood />
           </div>
 
-          <Stats />
+          <OnboardingModal />
         </div>
-
-        <OnboardingModal />
       </Container>
     </div>
   )
